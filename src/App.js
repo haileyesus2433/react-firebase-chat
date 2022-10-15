6d01bc872abb307dee46982813357476745143bc
Haileyesus Ayanaw <85413826+haileyesus2433@users.noreply.github.com>
2022-10-15 09:36:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function SignIn() {
 
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign In With Google</button>
       <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
   )
